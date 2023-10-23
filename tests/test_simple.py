4181bd9545fc2a9f98f661bf0729ab567e5039f0
Jacky Chen <jacky.chen@nextlink.com.tw>
2023-10-23 18:13:18 +0800

    tests: check chains have at least 2 certificates

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -7,7 +7,7 @@ from make_certificate_chain import utils
 def test_simple(example_com_cert: bytes):
     cert = utils.read_x509_certificates(example_com_cert)
     chain = list(solver.solve_cert_chain(cert[0], include_root_ca=True))
-    assert len(chain) > 0
+    assert len(chain) > 2 # Expect more than 2 as root CA is included
     sans = [
         san.value
         for san in chain[0].extensions.get_extension_for_class(x509.SubjectAlternativeName).value
@@ -18,7 +18,7 @@ def test_simple(example_com_cert: bytes):
 def test_pkcs7(epki_com_tw_cert: bytes):
     certs = utils.read_pkcs7_certificates(epki_com_tw_cert)
     chain = list(solver.solve_cert_chain(certs[0], include_root_ca=False))
-    assert len(chain) > 0
+    assert len(chain) > 1
     sans = [
         san.value
         for san in chain[0].extensions.get_extension_for_class(x509.SubjectAlternativeName).value
@@ -32,7 +32,7 @@ def test_pkcs12(example_com_cert_pkcs12: bytes):
         b'12345678'
     )
     chain = list(solver.solve_cert_chain(certs[0], include_root_ca=False))
-    assert len(chain) > 0
+    assert len(chain) > 1
     sans = [
         san.value
         for san in chain[0].extensions.get_extension_for_class(x509.SubjectAlternativeName).value
