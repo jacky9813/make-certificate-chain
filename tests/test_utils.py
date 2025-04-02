7f2fc21f0d760a7217199f79590d670a1f2985a0
Jacky Chen <jacky9813@hotmail.com>
2025-04-02 17:03:43 +0800

    test: add basic ocsp test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,14 @@
 from make_certificate_chain import utils
+from make_certificate_chain import solver
 
 def test_get_system_ca():
     system_ca = utils.get_system_ca()
     assert len(system_ca) > 0
+
+
+def test_ocsp(example_com_cert: bytes):
+    cert = utils.read_x509_certificates(example_com_cert)[0]
+    issuer = solver.get_issuer_certificate(cert)[
+        cert.issuer.rfc4514_string()][0]
+
+    utils.verify_against_ocsp(cert, issuer)
