48fb724e9cfcc418fd056c91cd01d798215b5f97
Jacky Chen <jacky9813@hotmail.com>
2025-04-10 18:25:11 +0800

    feat: command for verifying cert chain

--- make_certificate_chain/cli/__init__.py
+++ make_certificate_chain/cli/__init__.py
@@ -6,4 +6,5 @@ from . import cmd_get_issuer
 from . import cmd_list_system_ca
 from . import cmd_output_only
 from . import cmd_split_certs
+from . import cmd_verify_chain
 from . import cmd_version
---
+++ make_certificate_chain/cli/cmd_verify_chain.py
@@ -0,0 +1,87 @@
+import itertools
+import sys
+import typing
+import logging
+
+import click
+from cryptography.hazmat.primitives.serialization import Encoding
+
+from .cli import cli
+from . import common
+from .. import utils
+from .. import solver
+
+logger = logging.getLogger(__name__)
+
+
+@cli.command()
+@click.argument(
+    "certificate_in",
+    type=click.File(mode="rb"),
+    nargs=-1
+)
+@click.option(
+    "--capath",
+    help="The path where CA certificates store at. "
+    "Can be a directory containing multiple X.509 files or a single X.509 "
+    "file. Default store path depends on the operating system or OpenSSL "
+    "configuration."
+)
+@click.option(
+    "--skip-ocsp",
+    help="Skip certificate revoke check via OCSP",
+    is_flag=True,
+    default=False
+)
+def verify_chain(
+    certificate_in: typing.Tuple[typing.BinaryIO, ...],
+    capath: typing.Optional[str],
+    skip_ocsp: bool
+):
+    """
+        Output certificate chain to stdout.
+
+        Leave certificate_in blank for reading from stdin.
+    """
+    if skip_ocsp:
+        logger.warning(
+            "Will not check certificate revocation with OCSP stapling"
+        )
+
+    cert_chain = list(itertools.chain(*[
+        utils.read_x509_certificates(cert_fd.read())
+        for cert_fd in certificate_in
+    ]))
+    if not cert_chain:
+        logger.critical("No certificate found in file.")
+        sys.exit(1)
+
+    logger.info("Read %d certificates", len(cert_chain))
+
+    ca_certs = utils.get_system_ca(capath or None)
+
+    if cert_chain[-1].subject != cert_chain[-1].issuer:
+        # add root ca to the end
+        ca_name = cert_chain[-1].issuer.rfc4514_string()
+        root_ca_cert = ca_certs.get(ca_name)
+        if root_ca_cert:
+            ca_cert = root_ca_cert[0]
+            logger.info(
+                'Using Root CA certificate "%s"',
+                ca_cert.subject.rfc4514_string()
+            )
+            cert_chain.append(ca_cert)
+        else:
+            logger.warning('CA certificate for "%s" not found', ca_name)
+
+    for subject, issuer in zip(cert_chain[:-1], cert_chain[1:]):
+        logger.info("=" * common.PADDING_LENGTH)
+        logger.info("Verifying:")
+        logger.info("Subject: %s", subject.subject.rfc4514_string())
+        logger.info("Issuer: %s", issuer.subject.rfc4514_string())
+        solver.verify_certificate(
+            subject, issuer, skip_ocsp_verification=skip_ocsp
+        )
+        logger.info("")
+        logger.info("Verify: OK")
+        logger.info("=" * common.PADDING_LENGTH)
