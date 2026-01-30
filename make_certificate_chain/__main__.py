3721c1c509bdb34ad220c93771fd93c56463bba0
Jacky Chen <jacky9813@hotmail.com>
2026-01-30 12:07:57 +0800

    refactor: change how logging is setup

--- make_certificate_chain/__main__.py
+++ make_certificate_chain/__main__.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python3
+from .cli import cli
 import logging
 
-# Logging config must be executed before importing cli
-logging.basicConfig(
-    format="[%(asctime)s] %(filename)s:%(lineno)-3d %(levelname)s: %(message)s",
-    level=logging.INFO,
-    handlers=[
-        logging.StreamHandler()
-    ]
-)
 
-from .cli import cli
+LOG_FMT = "[%(asctime)s] %(filename)s:%(lineno)-3d %(levelname)s: %(message)s"
+LOG_LEVEL = logging.INFO
+
+root_logger = logging.root
+
 
 def main():
+    # Configuring logging
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter(fmt=LOG_FMT))
+    handler.setLevel(LOG_LEVEL)
+    root_logger.addHandler(handler)
+    root_logger.setLevel(LOG_LEVEL)
+    for _, logger in logging.root.manager.loggerDict.items():
+        if isinstance(logger, logging.Logger):
+            # Removing custom handlers created by modules
+            logger.handlers.clear()
+            # While make sure the logs will reach root logger
+            logger.propagate = True
+            logger.setLevel(LOG_LEVEL)
     cli()
 
+
 if __name__ == "__main__":
-    cli()
+    main()
